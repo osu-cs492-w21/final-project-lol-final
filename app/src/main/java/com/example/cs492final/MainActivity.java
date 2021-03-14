d71a8c4921a9430e168820844809cfc8cf820edf
eugeneyjy <yonge@oregonstate.edu>
2021-03-13 21:30:49 -0800

    Skeleton for filter menu

--- app/src/main/java/com/example/cs492final/MainActivity.java
+++ app/src/main/java/com/example/cs492final/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cs492final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] tagArray = getResources().getStringArray(R.array.tag_array);
+        Spinner tagSpinner = findViewById(R.id.tag_spinner);
+        ArrayAdapter<String> tagAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, R.id.tv_spinner, tagArray);
+        tagAdapter.setDropDownViewResource(R.layout.spinner_item);
+        tagSpinner.setAdapter(tagAdapter);
+
+        String[] difficultyArray = getResources().getStringArray(R.array.difficulty_array);
+        Spinner difficultySpinner = findViewById(R.id.difficulty_spinner);
+        ArrayAdapter<String> difficultyAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, R.id.tv_spinner, difficultyArray);
+        difficultyAdapter.setDropDownViewResource(R.layout.spinner_item);
+        difficultySpinner.setAdapter(difficultyAdapter);
+
+        String[] partypeArray = getResources().getStringArray(R.array.partype_array);
+        Spinner partypeSpinner = findViewById(R.id.partype_spinner);
+        ArrayAdapter<String> partypeAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, R.id.tv_spinner, partypeArray);
+        partypeAdapter.setDropDownViewResource(R.layout.spinner_item);
+        partypeSpinner.setAdapter(partypeAdapter);
     }
 }
